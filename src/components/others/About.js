f14e4b20b522f6411403e8b21b6feac4c8df70dc
QuizBlogRW <quizblog.rw@gmail.com>
2023-10-20 00:16:18 +0200

    first commit

---
+++ src/components/others/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <div className="jumbotron m-md-5 py-1 text-center">
+                <h1 className="display-5 pt-3 font-weight-bold">Amahirwe</h1>
+                <p className="lead">
+                    Amahirwe is a web application that helps people to buy, sell or rent anything. It gives people good time to search, plan and decide what to buy, sell or rent. It also gives information about jobs and scholarships and tenders. Amahirwe is a part of Elysium Group Ltd.
+                </p>
+
+                <hr className="my-2" />
+                <p>Reach us on <strong>
+                    <a href="mailto:amahirwe2023@gmail.com?subject=Contact%Amahirwe">amahirwe2023@gmail.com</a>
+                </strong> for further details.</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
