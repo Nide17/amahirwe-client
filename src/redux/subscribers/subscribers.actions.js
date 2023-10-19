4cb0877514f8ef80ccbdb95c6378bd928b7a05eb
QuizBlogRW <quizblog.rw@gmail.com>
2023-10-20 01:34:14 +0200

    renew packages

--- src/redux/subscribers/subscribers.actions.js
+++ src/redux/subscribers/subscribers.actions.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { tokenConfig } from '../auth/auth.actions'
 import { returnErrors } from "../error/error.actions";
 
-
 const axiosInstance = axios.create({
   baseURL: 'https://amahirwe-server.onrender.com',
 });
